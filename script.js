3483b29b09baeae93206abb0c2b83b5e6f32725b
Jakub Olszewski <jakubolszewski95@yahoo.com>
2024-05-05 15:17:29 +0200

    added another scene, added simple animation

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      if (entry.target.classList.contains("left-door")) {
-        entry.target.classList.add("animation-left"); // Add animation class for left door when in view
-      } else if (entry.target.classList.contains("right-door")) {
-        entry.target.classList.add("animation-right"); // Add animation class for right door when in view
-      }
-    } else {
-      if (entry.target.classList.contains("left-door")) {
-        entry.target.classList.remove("animation-left"); // Remove animation class for left door when out of view
-      } else if (entry.target.classList.contains("right-door")) {
-        entry.target.classList.remove("animation-right"); // Remove animation class for right door when out of view
-      }
-    }
-  });
-});
+const sectionEls = document.querySelectorAll(".animate");
 
-const observedElements = document.querySelectorAll(".animate");
-observedElements.forEach((el) => {
-  observer.observe(el);
-  
-  // Trigger animation for elements initially in view
-  if (el.getBoundingClientRect().top < window.innerHeight) {
-    if (el.classList.contains("left-door")) {
-      el.classList.add("animation-left");
-    } else if (el.classList.contains("right-door")) {
-      el.classList.add("animation-right");
+const options = {
+	rootMargin: "10%",
+};
+
+const observer = new IntersectionObserver(entries=>{
+	entries.forEach(entry=>{
+    const isLeftDoor = entry.target.classList.contains("left-door");
+    const isRightDoor = entry.target.classList.contains("right-door");
+    const isWaterflow = entry.target.classList.contains("waterflow")
+    const isBoat = entry.target.classList.contains("boat")
+		
+    if(entry.isIntersecting && isLeftDoor){
+			entry.target.classList.add("animation-left");
+		}else{
+			entry.target.classList.remove("animation-left");
+		
+    }if(entry.isIntersecting && isRightDoor)
+    entry.target.classList.add("animation-right");
+    else{
+      entry.target.classList.remove("animation-right")
+    
+    }if(entry.isIntersecting && isWaterflow)
+    entry.target.classList.add("squiggle")
+    else{
+      entry.target.classList.remove("squiggle")
+    
+    } if(entry.isIntersecting && isBoat)
+    entry.target.classList.add("boatAnimation")
+    else{
+      entry.target.classList.remove("boatAnimation")
     }
-  }
-});
+
+	});
+}, options);
+
+sectionEls.forEach(el=>observer.observe(el));
